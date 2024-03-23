fa1317056678d98cd3f4818b3bc35f8c1bf2020e
zul <fahrinzul77@gmail.com>
2024-03-24 02:42:14 +0800

    first fil

---
+++ index.php
@@ -0,0 +1,65 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="css/bootstrap-grid.css">
+        <title>Login</title>
+        <link rel="stylesheet" href="css/index.css">
+    </head>
+    <body>
+        <div class="container">
+            <center>
+            <img class="img-responsive" src="img/logo-kv.png">
+        </center>
+        <br>
+        </div>
+        <!-- <center> -->
+        <div class="col-sm-4">
+        <div class="d-flex justify-content-center">
+            <img  class="img-fluid" src="img/admin.png" width="45%">
+        </div>
+        <br>
+        <br>
+        <br>
+        <div class="d-flex justify-content-center">
+            <a href="pelajar-login.html"><input class="b1" type="button" value="Admin"></a>
+        </div>
+        </div> 
+        <div class="col-sm-4">
+            <div class="d-flex justify-content-center">
+            <img  class="img-fluid" src="img/teacher.png" width="70%">
+        </div>
+        <br>
+        <!-- <br> -->
+        <div class="d-flex justify-content-center">
+            <a href="pelajar-login.html"><input class="b1" type="button" value="Guru"></a>
+            </div>
+        </div> 
+        <div class="col-sm-4">
+            <div class="d-flex justify-content-center">
+            <img  class="img-fluid" src="img/student.png" width="50%">
+        </div>
+        <br>
+        <br>
+        <div class="d-flex justify-content-center">
+            <a href="pelajar-login.php"><input class="b1" type="button" value="Pelajar"></a>
+            </div>
+        </div> 
+    <!-- </center> -->
+    
+    <footer class="col-sm-12">
+        <br>
+        <br>
+        <br>
+        <!-- Copyright -->
+        <div class="d-flex justify-content-center p-5" style="background-color: grey;">
+          © 2024 Copyright:
+        </div>
+        <!-- Copyright -->
+      </footer>
+      
+    </body>
+</html>
